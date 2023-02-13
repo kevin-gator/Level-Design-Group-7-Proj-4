ff276cbd6ddd5f29d5372b7f677a7d8dbfd98af9
kevin-gator <116971593+kevin-gator@users.noreply.github.com>
2023-02-13 14:29:21 -0500

    Raft movement
    ...

---
+++ Assets/Scripts/Raft.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raft : MonoBehaviour
+{
+    public bool isMoving;
+
+    [SerializeField]
+    private float _speed;
+
+    [SerializeField]
+    private Rigidbody _rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        isMoving = true;
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(isMoving)
+        {
+            _rb.velocity = new Vector3(_rb.velocity.x, _rb.velocity.y, _speed);
+        }
+        else
+        {
+            _rb.velocity = Vector3.zero;
+        }
+    }
+
+    void StopMoving()
+    {
+        isMoving = false;
+    }
+}
